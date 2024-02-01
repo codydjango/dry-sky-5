002523a9d0c7ac6a47eaefbb5fdb995e192ba9b9
django <django@2ccb541778be>
2024-02-01 00:13:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-sky-5",
+  url: "https://dry-sky-5.botics.co",
+  api: "https://dry-sky-5.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
